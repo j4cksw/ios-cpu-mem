e394d1fbc6c04c13f17517004dd57b8d429e7276
Prayoch Rujira <prayoch@clockupstudio.com>
2020-08-19 22:15:16 +0700

    Initial Commit

---
+++ JMonitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JMonitor
+//
+//  Created by Prayoch Rujira on 19/8/2563 BE.
+//  Copyright © 2563 j4cksw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
